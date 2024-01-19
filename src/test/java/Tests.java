babce5103aee97b93acabeace7d5dcf3e19ec318
Denis Volchenko <volchenko.di@infotek.ru>
2024-01-19 11:04:29 +0300

    Добавил вебдрайвер для firefox

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -5,6 +5,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import pageobjects.LoginPage;
 
 //import static com.codeborne.selenide.Selenide.open;
@@ -14,14 +15,17 @@ public class Tests {
 
     @Before
     public void setUp(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
     }
 
     @org.junit.Test
     public void LoginTest(){
         Selenide.open(URL.MAIN_URL);
         LoginPage loginPage = new LoginPage(driver);
+
         loginPage.setLogin(Accounts.ADMIN_EMAIL);
         loginPage.setPassword(Accounts.ADMIN_PASSWORD);
         loginPage.clickLoginButton();
