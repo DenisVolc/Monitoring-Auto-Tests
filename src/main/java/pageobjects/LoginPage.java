747aa805db758b155e71d9a97781d7a584929454
Denis Volchenko <volchenko.di@infotek.ru>
2024-02-01 10:56:56 +0300

    убрал все упоминания selenium оставил только selenide

--- src/main/java/pageobjects/LoginPage.java
+++ src/main/java/pageobjects/LoginPage.java
@@ -1,33 +1,34 @@
 package pageobjects;
 
 import com.codeborne.selenide.SelenideElement;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.How;
+
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class LoginPage   {
 
-    WebDriver driver;
+
+
     String checkout;
 
-    public LoginPage(WebDriver driver) {
-        this.driver = driver;
-    }
+
 //    @FindBy(how = How.XX, using = "YYY") // Comment
 //    private SelenideElement DDD;
 
-    @FindBy(how = How.ID, using = "user_email") // поле ввода логина
-    private SelenideElement loginInput;
-    @FindBy(how = How.ID, using = "user_password") // поле ввода пароля
-    private SelenideElement passwordInput;
-    @FindBy(how = How.ID, using = "user_remember_me") // чек-бокс "Оставаться в системе"
-    private SelenideElement rememberMeCheckbox;
-    @FindBy(how = How.XPATH, using = "//button[contains(text(),'Войти')]") // Кнопка "Войти ->"
-    private SelenideElement loginButton;
-    @FindBy(how = How.CLASS_NAME, using = "px-2 text-[#ffffff] fixed left-3 bottom-4 z-50") // Кнопка-бутерброд в нижнем левом углу
-    private SelenideElement sandwichButton;
-    @FindBy(how = How.ID, using = "sidebar") // Синяя полоса слева с лого компании
-    private SelenideElement sidebar;
+//    @FindBy(how = How.ID, using = "user_email")
+    private SelenideElement loginInput = $(byId("user_email"));// поле ввода логина
+
+    private SelenideElement passwordInput = $(byId("user_password"));// поле ввода пароля
+
+    private SelenideElement rememberMeCheckbox =  $(byId("user_remember_me"));; // чек-бокс "Оставаться в системе"
+
+    private SelenideElement loginButton = $(byXpath("//button[contains(text(),'Войти')]"));// Кнопка "Войти ->"
+
+    private SelenideElement sandwichButton = $(byClassName("px-2 text-[#ffffff] fixed left-3 bottom-4 z-50")); // Кнопка-бутерброд в нижнем левом углу
+
+    private SelenideElement sidebar = $(byId("sidebar")); // Синяя полоса слева с лого компании
 
 
     public void setLogin(String login){//ввод имэила
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,31 +1,25 @@
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import constatns.Accounts;
 import constatns.URL;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Before;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import pageobjects.LoginPage;
 
-//import static com.codeborne.selenide.Selenide.open;
 
 public class Tests {
-    WebDriver driver;
+
 
     @Before
     public void setUp(){
-//        WebDriverManager.chromedriver().setup();
-//        driver = new ChromeDriver();
-        WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
 
     }
 
     @org.junit.Test
     public void LoginTest(){
-        LoginPage loginPage = new LoginPage(driver);
-        Selenide.open(URL.MAIN_URL);//TODO прописать параметр  --allowed-ips
+//        LoginPage loginPage = new LoginPage(driver);
+//        Configuration.browser = "firefox";
+        Selenide.open(URL.MAIN_URL);
+        LoginPage loginPage = new LoginPage();
         loginPage.setLogin(Accounts.ADMIN_EMAIL);
         loginPage.setPassword(Accounts.ADMIN_PASSWORD);
         loginPage.clickLoginButton();
